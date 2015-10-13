9ccfe8e6894e57b1ebf3089dd433327420ea40e6
wangdehuai <wangdehuai@me.com>
2015-10-13 15:00:51 +0800

    Initial Commit

---
+++ UPLOADIMG/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  UPLOADIMG
+//
+//  Created by 王德怀 on 15/10/13.
+//  Copyright © 2015年 王德怀. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
